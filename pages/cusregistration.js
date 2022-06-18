243782a6833c1fa9ac268c9f4db037b249df23d3
dumidu1998 <dumiduraj@gmail.com>
2022-06-19 01:18:36 +0530

    ch

--- pages/cusregistration.js
+++ pages/cusregistration.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import CustomerReg from '../components/CustomerReg'
+
 function cusregistration() {
     return (
         <>
